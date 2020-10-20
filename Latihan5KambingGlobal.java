185e1e9effc5dc7e6ab59f2fc68142b229665da6
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-10-20 21:35:35 +0700

    Add files via upload

---
+++ Latihan5KambingGlobal.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan5kambingglobal;
+
+/**
+ *
+ * @author damai
+ */
+public class Latihan5KambingGlobal {
+//variable jumlahKambing menjadi variable instance
+    int jumlahKambing = 88;
+    
+//method untuk menampilkan jumlah kambing
+    
+    public void getJumlahKambing(){
+    System.out.println("Jumlah kambing: " + jumlahKambing); 
+  
+    }
+   public void tambahKambing(){
+      jumlahKambing = jumlahKambing + 5;
+       System.out.println("jumlah kambing setelah ditambah: " +jumlahKambing);
+    
+    }
+    
+  public static void main(String[] args){
+    kambingGlobal kambingSusu = new kambingGlobal();
+    
+    //menampilkan jumlah kambing yang ada saat program pertama x berjalan
+    kambingSusu.getJumlahKambing();
+    
+    //menambah satu kambing
+    kambingSusu.tambahKambing();
+    
+    //menampilkan jumlah kambing yang ada
+    kambingSusu.getJumlahKambing();
+    
+
+  }
+
+}
